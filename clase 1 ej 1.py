1c80482e1985a8603473d0e674f75d279444a5ef
Gustavo M. Minchiotti <Gustavominchiotti@gmail.com>
2022-06-01 14:37:39 -0300

    solo subo un archivo practica

---
+++ clase 1 ej 1.py
@@ -0,0 +1,11 @@
+# tema 1
+mensaje1='Hola, soy '
+mensaje2='Estoy empezando el curso de Python'
+mensaje3="Espero aprender mucho"
+nombre="Gustavo Marcelo Minchiotti"
+print(mensaje1,nombre)
+print(mensaje2)
+print(mensaje3)
+print()
+var = '¡¡Hola Mundo!!'
+print(var)
