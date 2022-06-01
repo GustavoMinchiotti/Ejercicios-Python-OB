0ad3649f188e4bffcbbd53b48b5bfc54e5c31098
Gustavo M. Minchiotti <Gustavominchiotti@gmail.com>
2022-06-01 15:15:16 -0300

    corrección ortográfica

---
+++ diccionario ejemplo de clase tema 3.py
@@ -0,0 +1,9 @@
+#diccionario ejemplo de clase tema 3 Roman
+diccionario = { 
+	"clave": "valor" ,
+	"perro": "animal que ladra" ,
+	"numero": 15
+	}
+
+print(diccionario)
+print(diccionario["numero"])
