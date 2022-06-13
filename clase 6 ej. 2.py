9ee8d3ae84315c1f1264dc9669d60dad5f97cefa
Gustavo M. Minchiotti <Gustavominchiotti@gmail.com>
2022-06-13 08:59:11 -0300

    agrego ejercicio clase 8 nº2 vacio para completar

--- clase 6 ej. 2.py
+++ clase 6 ej. 2.py
@@ -4,53 +4,53 @@ que tenga como atributos su nombre y su nota.
 Deberéis de definir los métodos para inicializar sus atributos, imprimirlos y mostrar un mensaje
 con el resultado de la nota y si ha aprobado o no.'''
 
+
 class Alumno():
     nombre = 'Juan'
     nota = 0
+
     def calificacion(nota):
         """ Saber si aprueba o no la materia"""
         if nota <= 6:
-            print(Alumno.nombre,' No aprueba')
+            print(Alumno.nombre, ' No aprueba')
         else:
-            print(Alumno.nombre,' aprobado')
+            print(Alumno.nombre, ' aprobado')
 
-        
+
 class Alumno2():
-    
-    def __init__(self,nombre,nota):
-        self.nombre = nombre            #digo al constructor que tome el parametro
+
+    def __init__(self, nombre, nota):
+        self.nombre = nombre  # digo al constructor que tome el parametro
         self.nota = nota
         self.resultado = 0
-    
+
     def califica(self):
         """ Saber si aprueba o no la materia"""
-        res = self.resultado + self.nota 
+        res = self.resultado + self.nota
         if res <= 6:
             print('No aprueba')
         elif res == 10:
-            print('Excelente')   
+            print('Excelente')
         else:
             print('aprueba')
-            
-            
-a1= Alumno2('Jaimito',3)
-a2= Alumno2('Wolfgang',10)
-a3= Alumno2('Charo',9)
-print(a1.nombre, a1.nota) #muy importante pasar objeto + . + parametro
 
-print('*-**-*-*'*10)
 
-print('Alumno: ',a1.nombre,'\nNota: ',a1.nota)
-a1.califica()   #llamo a la funcion 
+a1 = Alumno2('Jaimito', 3)
+a2 = Alumno2('Wolfgang', 10)
+a3 = Alumno2('Charo', 9)
+print(a1.nombre, a1.nota)  # muy importante pasar objeto + . + parametro
 
-print('*-**-*-*'*10)
+print('*-**-*-*' * 10)
 
-print('Alumno: ',a2.nombre,'\nNota: ',a2.nota)
+print('Alumno: ', a1.nombre, '\nNota: ', a1.nota)
+a1.califica()  # llamo a la funcion
+
+print('*-**-*-*' * 10)
+
+print('Alumno: ', a2.nombre, '\nNota: ', a2.nota)
 a2.califica()
 
-print('*-**-*-*'*10)
+print('*-**-*-*' * 10)
 
-print('Alumno: ',a3.nombre,'\nNota: ',a3.nota)
+print('Alumno: ', a3.nombre, '\nNota: ', a3.nota)
 a3.califica()
-
-
---
+++ clase 8 ej. 2.py
@@ -0,0 +1,5 @@
+# En este segundo ejercicio, tendréis que crear un archivo py y dentro crearéis una clase Vehículo,
+# haréis un objeto de ella, lo guardaréis en un archivo y luego lo cargamos.
+
+'''en este ejercicio necesito revisar clases y seguir aprendiendo sobre archivos, en el ejercicio resuelto
+me falto mucho para saber'''
